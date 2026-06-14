924f0943590676e000ea0bddf39b7b10915ba646
Tu Nombre <tu@email.com>
2026-06-14 11:40:09 +0200

    refactor: reorganizar estructura a VSA

---
+++ app/(auth)/login/page.js
@@ -0,0 +1,9 @@
+import LoginForm from "./components/LoginForm"
+
+export default function Login() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-50">
+      <LoginForm />
+    </main>
+  )
+}
---
+++ app/(dashboard)/dashboard/page.js
@@ -0,0 +1,9 @@
+import DashboardView from "./components/DashboardView"
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-600 to-blue-400">
+      <DashboardView />
+    </main>
+  )
+}
