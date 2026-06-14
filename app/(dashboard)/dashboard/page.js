05281e27274600b9b3960d115340e4deb46b06f8
Tu Nombre <tu@email.com>
2026-06-14 18:24:11 +0200

    eliminar carpetas duplicadas de navi

--- app/(dashboard)/dashboard/page.js
+++
@@ -1,9 +0,0 @@
-import DashboardView from "./components/DashboardView"
-
-export default function Dashboard() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-blue-600 to-blue-400">
-      <DashboardView />
-    </main>
-  )
-}
